87ccbc1c93561fb08793d65cec5706dc4944d322
Artem Yankov <artem@badgeville.com>
2012-05-16 00:05:10 -0700

    ability to specify host and port for kafka server from a command line

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var help = [
     "Starts a juggernaut server using the specified command-line options",
     "",
     "options:",
+    "  --kafka-host  HOST  HOST for kafka server. Default: localhost.",
+    "  --kafka-port  PORT  Port that the kafka server is listening on. Default: 9092.",
     "  --port   PORT       Port that the proxy server should run on",
     "  --silent            Silence the log output",
     "  -h, --help          You're staring at it"
@@ -16,6 +18,9 @@ var help = [
 if (argv.h || argv.help) {
   return util.puts(help);
 }
+
+process.env.kafka_host = argv['kafka-host'] || "localhost";
+process.env.kafka_port = argv['kafka-port'] || 9092;
 
 Juggernaut = require("./index");
 Juggernaut.listen(argv.port);
